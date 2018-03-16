02dee8e22fe78b2d74d2fc694b8e3b5de9d4e857
Tiago Hernandes <tiago.hernandes@atual.local>
2018-03-16 14:29:03 -0300

    Alteração no arquivo principal da aplicação.

--- src/main/java/com/thernandes/api/MyFirstProjectApplication.java
+++ src/main/java/com/thernandes/api/MyFirstProjectApplication.java
@@ -1,5 +1,7 @@
 package com.thernandes.api;
 
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +10,6 @@ public class MyFirstProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyFirstProjectApplication.class, args);
+		System.out.println("My first project...");
 	}
 }
